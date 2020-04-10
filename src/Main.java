c1e7d877703c6cd1fc50123e3f1e05daa397a7b5
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2020-04-10 15:18:05 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,25 @@ import java.io.*;
 import java.nio.file.Files; 
 import java.nio.file.*; 
 import java.util.Arrays;
+import javax.swing.*;  
 
-public class Main { 
+public class Main extends JFrame{ 
+    static JProgressBar relocationProgress;    
+    static JLabel progressLabel = new JLabel("File Relocation Progress");    
+    static int i=0,num=0; 
+    static int size = 4; //Number of folders  
+                  
+    public static void iterate(int increment){
+        int sz = i;
+        while (i < (sz+increment)) {   
+            i += 1;
+            relocationProgress.setValue(i);                  
+            try{Thread.sleep(50);}catch(Exception e){}
+        }
+        if(i == 100)
+            System.exit(0);
+        return;         
+    } 
     public static String[] removeStringFromArr(String[] arr, int index) { 
         if (arr == null || index < 0 || index >= arr.length)  
             return arr; 
@@ -54,8 +71,9 @@ public class Main {
               Files.move(Paths.get(System.getProperty("user.home")+"/Downloads/"+content[index]),
                          Paths.get(System.getProperty("user.home")+"/Downloads/"+folder+"/"+content[index]));  
            } 
-           content = removeStringFromArr(content, index);
+           content = removeStringFromArr(content, index);                       
         }
+        iterate(100/size);             
     }
     
     private static void createDirectory (String name) {
@@ -63,39 +81,38 @@ public class Main {
         File pathFile = new File(path);
         pathFile.mkdir();
     }
-    public static void main(String[] args) {  
+    public static void main(String[] args) { 
+        JFrame window = new JFrame ("JavaDFM");
+        JPanel panel = new JPanel();
         String content[] = getDownloadsContents();
+        //Initialize progress bar
+        relocationProgress = new JProgressBar(0,100);           
+        relocationProgress.setValue(0);    
+        relocationProgress.setStringPainted(true); 
+        //Add UI to the JPanel
+        panel.add(progressLabel);       
+        panel.add(relocationProgress);
+        //Add the UI Panel to the Window
+        window.add(panel);
+        window.setVisible(true);
+        window.setSize(250, 100);
+        //Manage the file relocation
         createDirectory("Texts");
         createDirectory("PDFs");
         createDirectory("Word");
         createDirectory("Compressed");
         //Get text files and relocate them  
-        try {
-           relocateFiles (content, ".txt", "Texts");  
-        }
-        catch (Exception IOException) {
-           return;
-        }
+        try {relocateFiles (content, ".txt", "Texts");  }
+        catch (Exception IOException) {return;}
         //Get compressed files and relocate them
-        try {
-           relocateFiles (content, ".zip", "Compressed");  
-        }
-        catch (Exception IOException) {
-           return;
-        }
+        try {relocateFiles (content, ".pdf", "PDFs");  }
+        catch (Exception IOException) {return;}
         //Get PDFs and relocate them
-        try {
-           relocateFiles (content, ".pdf", "PDFs");  
-        }
-        catch (Exception IOException) {
-           return;
-        }
+        try {relocateFiles (content, ".zip", "Compressed");  }
+        catch (Exception IOException) {return;}
         //Get Word Docs and relocate them
-        try {
-           relocateFiles (content, ".docx", "Word");  
-        }
-        catch (Exception IOException) {
-           return;
-        }
+        try {relocateFiles (content, ".docx", "Word");  }
+        catch (Exception IOException) {return;}
+             
     } 
 } 
